f21b0bb34336497691f940943a80b3696807506b
Renuka <renu.bagave1901@gmail.com>
2024-04-23 15:19:56 +0530

    [Renuka]Add : Created class with required fields

---
+++ AddressBook/Contacts.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class Contacts 
+    {
+        public string FirstName {  get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+        public string Email { get; set; }
+        public long PhoneNumber { get; set; }
+
+        public string Display()
+        {
+            return $"FirstName is : {FirstName} LastName is : {LastName} Address is : {Address} City is :{City} State is :{State} ZipCode is : {ZipCode} Email is : {Email} PhoneNumber is : {PhoneNumber}";
+        }
+
+       
+
+        
+
+
+    }
+}
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -5,6 +5,9 @@
         static void Main(string[] args)
         {
             AddressBook a = new AddressBook();
+            Contacts contacts = new Contacts {FirstName = "Renuka", LastName = "Bagave", City = "Kankavli", State = "Maharashtra", ZipCode = 416602, Email = "renu.123@gmail.com",  PhoneNumber = 9425658955 };
+            
+            Console.WriteLine(contacts.Display());
         }
     }
 }
